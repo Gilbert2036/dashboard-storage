34c0adfd51614d0f1f2d08b5e37ad0d9d7c91b57
Gilbert2036 <126055747+Gilbert2036@users.noreply.github.com>
2026-03-24 08:49:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,76 +2,54 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+df = pd.read_csv("data_bersih.csv")
+
 st.title("Dashboard Monitoring Storage")
 
-# ======================
-# UPLOAD FILE
-# ======================
-
-uploaded = st.file_uploader(
-    "Upload file CSV / Excel",
-    type=["csv", "xlsx"]
-)
-
-if uploaded is not None:
-
-    if uploaded.name.endswith(".csv"):
-        df = pd.read_csv(uploaded)
-
-    else:
-        df = pd.read_excel(uploaded)
-
-else:
-    df = pd.read_csv("data_bersih.csv")
-
-
-# ======================
+# =====================
 # FILTER ITEM
-# ======================
+# =====================
 
 item = st.selectbox(
     "Pilih ITEM",
-    sorted(df["ITEM"].dropna().unique())
+    sorted(df["ITEM"].unique())
 )
 
 df_item = df[df["ITEM"] == item]
 
-
-# ======================
+# =====================
 # FILTER PARAMETER
-# ======================
+# =====================
 
 parameter = st.selectbox(
     "Pilih PARAMETER",
-    sorted(df_item["PARAMETER"].dropna().unique())
+    sorted(df_item["PARAMETER"].unique())
 )
 
 df_param = df_item[df_item["PARAMETER"] == parameter]
 
-
-# ======================
+# =====================
 # FILTER TANGGAL
-# ======================
+# =====================
 
 tanggal = st.multiselect(
-    "Pilih TANGGAL",
+    "Pilih Tanggal",
     sorted(df_param["TANGGAL"].unique()),
     default=sorted(df_param["TANGGAL"].unique())
 )
 
 df_final = df_param[df_param["TANGGAL"].isin(tanggal)]
 
-
-# ======================
+# =====================
 # TABEL
-# ======================
+# =====================
 
+st.subheader("Data")
 st.dataframe(df_final, use_container_width=True)
 
-
-# ======================
+# =====================
 # GRAFIK
-# ======================
+# =====================
 
 fig = px.line(
     df_final,
